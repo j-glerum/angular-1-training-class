a67da49b1d761d10621625ba011ffc18aa893099
J. Glerum <jglerum@valitics.com>
2016-11-09 14:12:43 -0700

    updated

--- src/app.js
+++ src/app.js
@@ -15,5 +15,45 @@ function AppController($scope) {
       affiliations: ['Jedi', 'Rebels'],
       light: true
     },
+    {
+      id: 2,
+      name: 'Luke Skywalker',
+      imageUrl: 'images/luke.png',
+      power: 8000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    },
+    {
+      id: 3,
+      name: 'Luke Skywalker',
+      imageUrl: 'images/luke.png',
+      power: 7000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    },
+    {
+      id: 4,
+      name: 'Luke Skywalker',
+      imageUrl: 'images/luke.png',
+      power: 6000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    },
+    {
+      id: 5,
+      name: 'Luke Skywalker',
+      imageUrl: 'images/luke.png',
+      power: 5000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    },
+    {
+      id: 6,
+      name: 'Luke Skywalker',
+      imageUrl: 'images/luke.png',
+      power: 4000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    },
   ]
 }
